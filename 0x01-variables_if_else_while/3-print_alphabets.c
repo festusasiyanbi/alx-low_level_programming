0cb485302081a06fbc9fe4e51149190e3f71b15a
Festus <festusasiyanbi80@gmail.com>
2022-10-14 13:57:10 -0700

    if_else_while loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,21 @@
-nclude <stdio.h>                                                                                                                                             
+#include <stdio.h>
 /**                                                                                                                                                            
- *  * main - Entry for the program                                                                                                                                
- *   *                                                                                                                                                             
- *    * Return: Always return zero (success)                                                                                                                        
- *     */                                                                                                                                                            
-int main(void)                                                                                                                                                 
+ * main - Entry for the program
+ *
+ * Return: Always return zero (success)
+ */
+int main(void)                                                                                                                            
+{                                                                                                                                                 
+char i;                                                                                                                                                        
+char j;                                                                                                                                                        
+for (i = 'a'; i <= 'z'; i++)                                                                                                                                   
 {                                                                                                                                                              
-	char i;                                                                                                                                                        
-	char j;                                                                                                                                                        
-	for (i = 'a'; i <= 'z'; i++)                                                                                                                                   
-	{                                                                                                                                                              
-		putchar(i);                                                                                                                                                    
-	}                                                                                                                                                              
-	for (j = 'A'; j <= 'Z'; j++) 
-	{
-		putchar(j);
-	}
-	putchar('\n');
-	return (0);
+putchar(i);                                                                                                                                                    
+}                                                                                                                                                              
+for (j = 'A'; j <= 'Z'; j++) 
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
 }
