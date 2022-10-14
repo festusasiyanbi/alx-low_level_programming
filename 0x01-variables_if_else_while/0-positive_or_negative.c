59b2f107e5d769bb9a19e1305ea8b6a8d2999dfc
Festus <festusasiyanbi80@gmail.com>
2022-10-14 04:33:06 -0700

    If_else_while loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point for the program
+ *
+ * return:by default returns -+0 respectively when n is <>= 0
+ */
+int main()
+{
+int n;
+if (n > 0){
+printf ("n is positive\n");
+} else {
+printf("n is negative\n");
+}else if (n == 0){
+printf ("n is zero\n");
+}
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My first if_else_while
