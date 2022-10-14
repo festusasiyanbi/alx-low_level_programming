50cf024330af5dcd937a953e231be3bd574bf806
Festus <festusasiyanbi80@gmail.com>
2022-10-14 11:31:02 -0700

    if_else_while loop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 srand(time(0));
 if (lastdigitof > 5)
 {
-printf("last digit of %d is %d and is  greater than 5\n", n, lastdigitof);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigitof);
 }
 else if (lastdigitof == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lastdigitof);
+printf("Last digit of %d is %d and is 0\n", n, lastdigitof);
 }
-else if (lastdigitof <6 && lastdigitof != 0)
+else if (lastdigitof < 6 && lastdigitof != 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigitof);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigitof);
 }
 return (0);
 }
