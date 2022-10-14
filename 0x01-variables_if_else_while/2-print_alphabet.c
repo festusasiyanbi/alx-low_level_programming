86a6fef2c3908d79e02e56a56bd905c164ec522b
Festus <festusasiyanbi80@gmail.com>
2022-10-14 13:24:39 -0700

    if_else_while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry for the program
+ *
+ * Return: Always return zero (success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; 1++)
+{
+putchar(i);
+}
+{
+putchar('\n');
+return (0);
+}
