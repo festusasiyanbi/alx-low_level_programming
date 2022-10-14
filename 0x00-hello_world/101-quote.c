db63312b11f436a2cab9705a4a9ae178aedb9a78
Festus <festusasiyanbi80@gmail.com>
2022-10-14 00:26:14 -0700

    Testing 'write()'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main- program entry point.
- * 
- * return:by default returns 1 on success.
+ * main- Entry point for the program
+ *
+ * return: returns 1 on success
  */
 int main(void)
-{ 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
