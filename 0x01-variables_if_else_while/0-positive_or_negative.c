0366543aaae9aaa4092803b2b9f899758338210e
Festus <festusasiyanbi80@gmail.com>
2022-10-14 05:15:30 -0700

    if_else_while loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,14 +8,15 @@
  */
 int main(void)
 {
-int n;
 srand (time(0));
-n = rand - RAND_MAX / 2;
+int n = rand - RAND_MAX / 2;
 if (n > 0){
 printf ("n is positive\n");
-} else {
-printf("n is negative\n");
-}else if (n == 0){
+} else if (n == 0){
 printf ("n is zero\n");
 }
+else {
+printf("n is negative\n");
+}
+return (0);
 }
