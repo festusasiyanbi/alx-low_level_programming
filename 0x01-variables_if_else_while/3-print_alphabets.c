4311d504b4df57c03c50a6e192cb16a111ed23bb
Festus <festusasiyanbi80@gmail.com>
2022-10-14 14:14:26 -0700

    if_else_while loop

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**                                                                                                                                                            
+/**
  * main - Entry for the program
  *
  * Return: Always return zero (success)
@@ -9,13 +9,13 @@ int main(void)
 char i;
 char j;
 
-for (i = 'a'; i <= 'z'; i++)
+for(i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
 }
 for (j = 'A'; j <= 'Z'; j++)
 {
-putchar(j);
+putchar(j)
 }
 putchar('\n');
 return (0);
