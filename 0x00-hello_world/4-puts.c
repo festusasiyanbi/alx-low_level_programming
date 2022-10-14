fc6beb2e2ebcb58334ad9c301e8be8000ac16ae4
Festus <festusasiyanbi80@gmail.com>
2022-10-13 22:43:46 -0700

    Hot c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * return:By detault returns zero if no errors otherwise returns non-zero value
  */
-int main()
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\n"Programming is like building a multilingual puzzle");
 return (0);
 }
