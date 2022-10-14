efc76debe3bd8ce98c582e84981cd88cb1775a60
Festus <festusasiyanbi80@gmail.com>
2022-10-14 14:59:36 -0700

    if_else_while loop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry for the program
+ *
+ * Return: Always return zero on success
+ */
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
