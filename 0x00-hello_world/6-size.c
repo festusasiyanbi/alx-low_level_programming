42ed1246b27ae215fc3af02532580d7924747da8
Festus <festusasiyanbi80@gmail.com>
2022-10-13 23:29:58 -0700

    Printing character sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("size of int: %d bytes\n", sizeof(int));
-printf("size of float: %d bytes\n", sizeof(float));
-printf("size of char: %d bytes\n", sizeof(char));
-printf("size of double: %d bytes\n", sizeof(double));
-
+printf("size of a char: %d bytes\n", sizeof(char));
+printf("size of an int: %d bytes\n", sizeof(int));
+printf("size of a long int: %d bytes\n", sizeof(long int));
+printf("size of a long long int: %d bytes\n", sizeof(long long int));
+printf("size of a float: %d bytes\n", sizeof(float));
 return (0);
 }
