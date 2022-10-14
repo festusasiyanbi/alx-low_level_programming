f66151e26b5a6115e3a3bfd06c01d0388f191094
Festus <festusasiyanbi80@gmail.com>
2022-10-14 13:49:01 -0700

    if_else_while loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>                                                                                                                                             
+/**                                                                                                                                                            
+ *  * main - Entry for the program                                                                                                                                
+ *   *                                                                                                                                                             
+ *    * Return: Always return zero (success)                                                                                                                        
+ *     */                                                                                                                                                            
+int main(void)                                                                                                                                                 
+{                                                                                                                                                              
+	char i;                                                                                                                                                        
+	char j;                                                                                                                                                        
+	for (i = 'a'; i <= 'z'; i++)                                                                                                                                   
+	{                                                                                                                                                              
+		putchar(i);                                                                                                                                                    
+	}                                                                                                                                                              
+	for (j = 'A'; j <= 'Z'; j++) 
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
