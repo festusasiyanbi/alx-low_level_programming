630e71574433fe8d9612ec5368b5ee401f417838
Festus <festusasiyanbi80@gmail.com>
2022-10-14 15:18:44 -0700

    if_else_while loop

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry for the program
+ *
+ * Return: Always return zero on success
+ */
+int main(void)
+{
+int i;
+
+char hexavalues[] = '0123456789abcdef';
+
+for (i = '0', i <= '16'; i++)
+{
+putchar([i]);
+}
+putchar('\n');
+return (0);
+}
