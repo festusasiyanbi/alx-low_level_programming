36d1d7f3d0dcd7b52d8b028f9dffac3341544faf
Festus <festusasiyanbi80@gmail.com>
2022-10-13 22:31:17 -0700

    Hot c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - Serves as entry point for the entire program
+ *
+ * Return - By detault returns zero if no error is encountered otherwise returns non-zero value
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
