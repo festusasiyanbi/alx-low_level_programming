2e04ef61be9a1f74080c26078650fc76969a4356
Festus <festusasiyanbi80@gmail.com>
2022-10-14 16:38:50 -0700

    if_else_while loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,16 @@ for (i = '0'; i <= '9'; i++)
 {
 for (j = k; j <= '9'; j++)
 {
+if (i != j)
+{
+putchar(i);
+putchar(j);
+}
 if (i == j)
 {
 continue;
 }
-if (i =='8' && j =='9')
+if (i == '8' && j == '9')
 {
 break;
 }
