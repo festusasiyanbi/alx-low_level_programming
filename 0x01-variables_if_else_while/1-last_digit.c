037a2c8af654f07f0661df59586fde0e8730448f
Festus <festusasiyanbi80@gmail.com>
2022-10-14 11:26:28 -0700

    if_else_while loop

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- entry point for the program
+ *
+ * return: zero if no errors otherwise non-zero value
+ */
+int main(void)
+{
+int n;
+int lastdigitof;
+n = rand() - RAND_MAX / 2;
+srand(time(0));
+if (lastdigitof > 5)
+{
+printf("last digit of %d is %d and is  greater than 5\n", n, lastdigitof);
+}
+else if (lastdigitof == 0)
+{
+printf("last digit of %d is %d and is 0\n", n, lastdigitof);
+}
+else if (lastdigitof <6 && lastdigitof != 0)
+{
+printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigitof);
+}
+return (0);
+}
