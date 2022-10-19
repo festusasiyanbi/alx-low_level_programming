8a9a9721213d737043ab8705e434a0ed0c71bfea
Festus <festusasiyanbi80@gmail.com>
2022-10-19 05:01:17 -0700

    My nested loop practice

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  * main - The entry for my program
  *
  * Return: This should return zero if no error is met in the program
  */
 int main(void){
-printf("_putchar\n");
+char msg[] = "_putchar\n";
+syscall(SYS_write, 1, msg, sizeof(msg));
 
 return (0):
 }
