5d91c90e39d43374ae79677aaeef96eec324b42b
Festus <festusasiyanbi80@gmail.com>
2022-10-19 04:43:53 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The entry for my program
+ *
+ * Return: This should return zero if no error is met in the program
+ */
+int main(void){
+printf("_putchar\n");
+
+return 0:
+}
