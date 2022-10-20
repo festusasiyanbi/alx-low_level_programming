4ead30cf0b204aeb056e446c7238f86084fa6723
Festus <festusasiyanbi80@gmail.com>
2022-10-20 07:27:28 -0700

    My nested loop task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,15 @@ int a, b, operation;
 for (a = 0; a <= 9; a++)
 {
 _putchar(48);
-}
 for (b = 0; b <= 9; b++)
+{
 operation = a * b;
 _putchar(44);
 _putchar(32);
 if (operation <= 9)
+{
 _putchar(32);
-_putchar(operation = 48);
+_putchar(operation + 48);
 }
 else
 {
