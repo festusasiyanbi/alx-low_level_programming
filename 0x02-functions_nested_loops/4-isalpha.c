7f2b235f6f920ec49020acd7ae3bcf1c73057848
Festus <festusasiyanbi80@gmail.com>
2022-10-20 03:47:53 -0700

    My nested loop task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _isalpha - checks if the char is in lower or uppercase
- * @c: this charater checks if it is in lowercase 
+ * _isalpha - checks checks code
+ * @c: this character checks if the program is in upper or lowercase
  *
  * Return: returns 1 on success
  */
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
+return 1;
 else
 {
-return (0);
+return 0;
 }
 }
