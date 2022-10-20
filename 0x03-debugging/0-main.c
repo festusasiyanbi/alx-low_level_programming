785cff80ad7265ee7b81230e0c585fa4de1b75a0
Festus <festusasiyanbi80@gmail.com>
2022-10-20 12:38:07 -0700

    My debugging task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negaive 
+ * main - tests function that prints if integer is + or -
  *
- * Return: always 0 on success
+ * Return: 0
  */
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
