a2ede40ec6587774cff27c6c6eb3b09359f60680
Festus <festusasiyanbi80@gmail.com>
2022-10-20 05:07:39 -0700

    My nested loop task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs -the function prints absolute value 
+ * @a: this charater computes the absolute value 
+ *
+ * Return: returns 0 on success
+ */
+int _abs(int a)
+{
+int k = a;
+int absolutevalue;
+if (k < 0)
+{
+k = k* (-1);
+return (absolutevalue);
+}
