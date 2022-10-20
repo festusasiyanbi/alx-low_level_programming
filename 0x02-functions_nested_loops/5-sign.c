5f2f093e4e44d800177a53f2ee6f790932d01d35
Festus <festusasiyanbi80@gmail.com>
2022-10-20 04:01:35 -0700

    My nested loop task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Checks for signs in the program
+ * @n: this character checks whether the program is +, - or 0
+ *
+ * Return: This returns + is n>1, - if n<1, 0 if n is 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf('+');
+return (1);
+}
+else if (n = 0)
+{
+printf('0');
+return (0);
+}
+else
+{
+printf('-1);
+return (-1);
+}
+}
