648d45c05b4697a8c8853b3e8699ec52112b35dc
Festus <festusasiyanbi80@gmail.com>
2022-10-20 06:44:42 -0700

    My nested loop task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - The program prints last digit of integer a
+ * @a: the character that computes the last digit of integer a
+ *
+ * Return: returns lastdigit on success
+ */
+int print_last_digit(int a)
+{
+int lastdigit;
+lastdigit = a % 10
+if (lastdigit < 0)
+{
+lastdigit = lastdigit* (-1);
+}
+_putchar(lastdigit + '0');
+return (lastdigit);
+}
