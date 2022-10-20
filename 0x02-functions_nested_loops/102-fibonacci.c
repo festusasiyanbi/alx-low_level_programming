7f419d6e19a61796d241dde43bafe61cda2df691
Festus <festusasiyanbi80@gmail.com>
2022-10-20 11:08:08 -0700

    My nested loop advanced task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - this programs returns fibonacci numbers
+ *
+ * Return: returns zero if no error is met
+ */
+int main(void)
+{
+int c;
+long int n1, n2, function;
+n1 = 1;
+n2 = 2;
+printf("%ld, %ld", n1, n2);
+for (c = 0; c < 48; c++)
+{
+function = n1 + n2;
+printf(", %ld", function);
+n1 = n2;
+n2 = function;
+}
+printf('\n');
+return (0);
+}
