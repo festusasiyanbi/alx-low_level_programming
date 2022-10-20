62e3f4fc8d50ec385538d5889f1aa76fe0d594b0
Festus <festusasiyanbi80@gmail.com>
2022-10-20 02:35:59 -0700

    My nested loop task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  *
  * Return: This should return zero if no error is met in the program
  */
-int main(void){
+int main(void)
+{
 int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 int i, z;
 z = sizeof(str) / sizeof(int);
