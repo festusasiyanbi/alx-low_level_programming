6cf6d3dc8cd38d5ef632d70716775ac1e57fc125
Festus <festusasiyanbi80@gmail.com>
2022-10-20 07:58:59 -0700

    My nested loop task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - this program adds 2 integers
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: returns addition of integers a and b
+ */
+int add(int a, int b)
+{
+return (a+b);
+}
