704d18b57a47420789299a8322f278db7d9328b5
Festus <festusasiyanbi80@gmail.com>
2022-10-21 16:39:50 -0700

    More nested loop task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - prints straight line n times
+ * Return: void
+ */
+void print_line(void)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
