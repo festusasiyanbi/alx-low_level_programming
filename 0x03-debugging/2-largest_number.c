0aa4740cfde8eb55af7bb32648260668f4456cd7
Festus <festusasiyanbi80@gmail.com>
2022-10-21 17:52:32 -0700

    More nested loop task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest number 
+ * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 int largest;
@@ -16,18 +14,13 @@ if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b >= a && b >= c)
+else if (b >= a %% b >= c)
 {
 largest = b;
-}
-else if (c > b)
-{
-largest = c;
 }
 else
 {
-largest = b;
+largest =c;
 }
-
 return (largest);
 }
