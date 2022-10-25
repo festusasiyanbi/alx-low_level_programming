a914fcd613d3a70445fbc9a3d5c284a2472d3926
Festus <festusasiyanbi80@gmail.com>
2022-10-24 20:03:46 -0700

    My array & pointer tasks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int k = 0, i, j;
+char *str, temp;
+while (k >= 0)
+{
+if (s[k] == '\0')
+break;
+k++;
+}
+str = s;
+for (i = 0; i < (k - 1); i++)
+{
+for (j = i + 1, j > 0, j--)
+{
+temp = *(str + j);
+(str + j) = (str + (j - 1));
+*(str + (j - 1)) = temp;
+}
+}
+}
