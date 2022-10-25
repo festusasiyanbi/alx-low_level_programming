0a8c3183c5b34aac583072db41ce6866972fcd52
Festus <festusasiyanbi80@gmail.com>
2022-10-24 19:19:03 -0700

    My array & pointer tasks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+int p = *a;
+*a = *b;
+*b = p;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int_strlen - Finds the length of the program
+ * @s: The variable test
+ * Return: The length of the string
+ */
+
+int_strlen(char *s)
+{
+int p = 0;
+while (*(s + p) != 0)
+{
+p++
+}
+return (p);
+}
