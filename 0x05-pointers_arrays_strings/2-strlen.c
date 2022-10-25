f9519744a13432d7fa87afa00cafd1eb0ce749b3
Festus <festusasiyanbi80@gmail.com>
2022-10-24 19:30:05 -0700

    My array & pointer tasks

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,10 +8,10 @@
 int_strlen(char *s)
 {
 int p = 0;
-/increment up to when the last char is NULL, \0/
+/increment up to when the last character is NULL, \0/
 while (*(s + p) != 0)
 {
-p++
+p++;
 }
 return (p);
 }
