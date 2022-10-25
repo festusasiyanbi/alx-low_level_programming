f6bd09ba3ee2423f184b97b10ef48c90206af9b7
Festus <festusasiyanbi80@gmail.com>
2022-10-24 21:26:00 -0700

    My array & pointer tasks

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - Print n elements of an array of integers
+ * @a: input array
+ * @n: number of elements in the array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int i = 0;
+for (; i < n; i++)
+{
+printf("%d", *(a = i));
+if (i != (n - 1))
+printf (",");
+}
+printf("\n");
+}
