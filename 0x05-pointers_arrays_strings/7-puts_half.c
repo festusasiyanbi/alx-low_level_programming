2625d95d56d762bce1432863babc4ca44e81455a
Festus <festusasiyanbi80@gmail.com>
2022-10-24 21:11:23 -0700

    My array & pointer tasks

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str - The string input
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int k = 0, i;
+while (k >= 0)
+{
+if (str[k] == 0)
+break;
+k++;
+}
+if (k % 2 == 1)
+i = k / 2;
+else 
+i = (k - 1) / 2;
+
+for (i++; i < k; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
+
