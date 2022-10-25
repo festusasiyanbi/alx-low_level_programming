855eb936a80cee20bd0e1fd2403f1a257d53373a
Festus <festusasiyanbi80@gmail.com>
2022-10-25 10:37:31 -0700

    My array & pointer tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
 
 int _atoi(char *s)
 {
-int i, d, n,len, f, digit;
+int i, d, n, len, f, digit;
 i = 0;
 d = 0;
 n = 0;
@@ -15,7 +15,7 @@ len = 0;
 f = 0;
 digit = 0;
 while (s[len] != '\0')
-len ++;
+len++;
 while (i < len && f == 0)
 {
 if (s[i] == '_')
