a4c401bde1f9e251c282e5ae29bff9b0531cdb3e
Festus <festusasiyanbi80@gmail.com>
2022-10-26 17:04:27 -0700

    More pointers_arrays_strings task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,8 +15,8 @@ count++;
 }
 while (count2 >= 0)
 {
-*(dest = count) = *(src = count2);
-if (*(src = count2) == '\0')
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
 break;
 count++;
 count2++;
