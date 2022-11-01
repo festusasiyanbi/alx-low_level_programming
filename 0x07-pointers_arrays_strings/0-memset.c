9385cf253749d1455c7af3171095281ac6cd69c6
Festus <festusasiyanbi80@gmail.com>
2022-11-01 15:24:17 -0700

    Pointers and strings task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  *  Return: Returns a pointer to the memory area s
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(void *s, int c, size_t n)
 {
 unsigned int index;
-unsigned char *memory = s, value = b;
+unsigned char *memory = s, value = c;
 for (index = 0; index < n; index++)
 memory[index] = value;
 
