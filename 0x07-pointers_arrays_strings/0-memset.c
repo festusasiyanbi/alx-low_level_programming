4e216bbe9974f246693b2aefe9e95305f7d32e98
Festus <festusasiyanbi80@gmail.com>
2022-11-01 14:48:51 -0700

    Pointers and strings task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - function fills the first n bytes of the memory area pointed
+ *  to by s with the constant byte b
+ *  Return: Returns a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int n;
+char *s = &n;
+char b;
+printf("The folowwing are bytes in n memory: %p/n", b, *s);
+
+return (*s);
+
+}
