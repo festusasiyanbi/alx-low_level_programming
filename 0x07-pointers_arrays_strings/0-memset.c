da2efca7f502b4ec9ef73dcefcd588f2e8626416
Festus <festusasiyanbi80@gmail.com>
2022-11-01 15:08:35 -0700

    Pointers and strings task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int n;
-char *s = &n;
-char b;
-printf("The folowwing are bytes in n memory: %p/n", b, *s);
+unsigned int i;
+unsigned char *memory = s, value = c;
+for (i = 0; i < n; i++)
+memory[i] = value;
 
-return (*s);
+return (&s);
 
 }
