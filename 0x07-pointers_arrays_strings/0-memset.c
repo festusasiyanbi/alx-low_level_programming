d590a6e7ac6f5ce4935cbd8ec55653b05677b6bc
Festus <festusasiyanbi80@gmail.com>
2022-11-01 15:17:26 -0700

    Pointers and strings task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int index;
-unsigned char *memory = s, value = c;
+unsigned char *memory = s, value = b;
 for (index = 0; index < n; index++)
 memory[index] = value;
 
