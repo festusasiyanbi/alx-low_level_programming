f5860ddde827ab552260f834e33486ee75c953d9
Festus <festusasiyanbi80@gmail.com>
2022-11-02 14:47:23 -0700

    My recursion task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - this function prints a string in reverse
+ * @s: the string input
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+if (s != '\0')
+{
+_putchar(*s);
+_puts_recoursion((s + 1);
+}
+else
+_putchar('\n');
+}
