80fd39c656121837009b200809494ec47100612d
Festus <festusasiyanbi80@gmail.com>
2022-11-02 15:14:08 -0700

    My recursion task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,14 +2,13 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string
- * Return: returns zero on success
+ * Return: returns the length of a string
  */
 
 int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
-}
 else
 return (1 + _strlen_recursion(s + 1));
 }
