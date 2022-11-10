dc2750f6296612dfca6ccc38c8b5358ed92e8acb
Festus <festusasiyanbi80@gmail.com>
2022-11-10 06:09:06 -0800

    More automatic and dynamic allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *p;
 unsigned int size1 = 0, size2 = 0, i;
 if (s1 == NULL)
-s1 = "";
+s1 = " ";
 if (s2 == NULL)
-s2 = "";
+s2 = " ";
 while (s1[size1] != '\0')
 {
 size1++;
@@ -25,7 +25,7 @@ size2++;
 }
 if (n > size2)
 n = size2;
-p = malloc((size1 + n + 1) * sizeof(char));
+p = malloc(sizeof(char) * (size1 + n + 1));
 if (p == NULL)
 return (0);
 for (i = 0; i < size1; i++)
