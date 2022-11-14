ccdb272f610015e1950334a1a0ca244d19da9c1c
Festus <festusasiyanbi80@gmail.com>
2022-11-14 09:16:16 -0800

    Structure and typedef usage in C programming

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,13 +13,13 @@ return;
 if (d->name == NULL)
 printf("Name: (Nil)\n");
 else
-printf("My name is %s\n", d->name);
+printf("Name: %s\n", d->name);
 if (d->age == NULL)
 printf("Age: (Nil)\n");
 else
-printf("I am %f\n years old", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (Nil)\n");
 else
-printf("My boss is %s\n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
