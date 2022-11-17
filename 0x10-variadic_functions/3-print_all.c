bba5f585ea836b2408f34358dd6d7f956408ede6
Festus <festusasiyanbi80@gmail.com>
2022-11-17 10:38:59 -0800

    Variadic functions in C programming

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,7 +22,7 @@ while (t_arg[j])
 {
 if (format[i] == t_arg[j] && c)
 {
-printf(" , ");
+printf(",");
 break;
 } j++;
 }
@@ -31,14 +31,11 @@ switch (format[i])
 case 'c':
 printf("%c", va_arg(valist, int)), c = 1;
 break;
-
 case 'i':
 printf("%d", va_arg(valist, int)), c = 1;
 break;
-
 case 'f':
 printf("%f", va_arg(valist, double)), c = 1;
-
 case 's':
 str = va_arg(valist, char *), c = 1;
 if (!str)
