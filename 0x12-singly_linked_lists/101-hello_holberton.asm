dc8ea2352b76c7ee167dd5fba9161c652037dca4
Festus <festusasiyanbi80@gmail.com>
2022-11-24 20:31:17 -0800

    Singly linked list in C programming

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+extern printf
+
+section .text
+global main
+
+main:
+ push rbp
+
+mov rdi, fmt
+mov rsi, msg
+mov rax, a
+call printf
+
+pop rbp
+
+mov rax, a
+ret
+
+section .data
+msg: db "Hellow, Holberton", a
+fmt: db "%s", 10, 0
