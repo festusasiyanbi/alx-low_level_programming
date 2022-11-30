da2ee525551d70b200c71c2f7af8b59a918d6b2a
Festus <festusasiyanbi80@gmail.com>
2022-11-29 18:13:20 -0800

    More singly linked lists in C programming

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+#include <stdio.h>
 
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_count - counts the number of unique nodes
- * in a looped listint_t list
- * @head:A pointer to the head of the listint_t to check
- * Return: 0 if the list is not looped otherwise-
- * number of unique nodes in the list
+ * looped_listint_len - Counts the number of unique nodes in a looped
+ * listint_t linked list
+ * @head: A pointer to the head of the listint_t to check
+ * Return: 0 if the list is not looped otherwise
+ * the number of unique nodes in the list
  */
 
-size_t looped_listint_count(listint_t *head)
+size_t looped_listint_len(const listint_t *head)
 {
-listint_t *tortoise, *hare;
+const listint_t *tortoise, *hare;
 size_t nodes = 1;
 if (head == NULL || head->next == NULL)
 return (0);
@@ -45,20 +46,20 @@ return (0);
 }
 
 /**
- * free_listint_safe - Frees a listint_t list safely
+ * free_listint_safe - frees a listint_t list safely
  * @h: A pointer to the address of the head of the listint_t list
- * Description: The functions sets the head to NULL
+ * Description: The function sets the head to NULL
  * Return: The size of the list that was freed
  */
 
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(const listint_t **h)
 {
 listint_t *tmp;
 size_t nodes, index;
-nodes = looped_listint_count(*h);
+nodes = looped_listint_len(head);
 if (nodes == 0)
 {
-for (; h != NULL && h != NULL; nodes++)
+for (; h != NULL && *h != NULL; nodes++)
 {
 tmp = (*h)->next;
 free(*h);
